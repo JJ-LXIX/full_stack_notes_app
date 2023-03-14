a854c9a5d6837355b05519f57d974afe1ed86071
Jagrath <jagrathshettylxix@gmail.com>
2023-03-14 18:58:25 +0530

    List of Todos added to UI

--- Components/ListTodo.tsx
+++ Components/ListTodo.tsx
@@ -1,7 +1,18 @@
+"use client ";
 import { useEffect, useState } from "react";
 
 function ListTodo() {
-  const [todos, setTodos] = useState([]);
+  const arr = [1, 2, 3, 4];
+  type DATA = [
+    {
+      todo_id: string;
+      description: string;
+    }
+  ];
+
+  type loopTodo = { todo_id: string; description: string };
+
+  const [todos, setTodos] = useState<DATA>();
   async function getTodos() {
     try {
       const response = await fetch("http://localhost:3000/api/todos");
@@ -14,9 +25,36 @@ function ListTodo() {
 
   useEffect(() => {
     getTodos();
+    // console.log(todos);
   }, []);
 
-  return <div className="w-full  h-[75vh]"></div>;
+  return (
+    <div className="w-full  h-[75vh]">
+      <table className="w-full border border-seperate">
+        <thead>
+          <tr>
+            <th>DESCRIPTION</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody className="border">
+          {todos?.map((todo: loopTodo) => {
+            // console.log(i, todo["description"]);
+            return (
+              <tr>
+                <td className=" p-2 ">{todo.description}</td>
+                <td className=" p-2 text-center">EDIT</td>
+                <td className=" p-2 text-center">DELETE</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div></div>
+      {/* <p>{todos[0].description}</p> */}
+    </div>
+  );
 }
 
 export default ListTodo;

8a2323aa6876938a55af59ad555067bd3d5a8297
Jagrath <jagrathshettylxix@gmail.com>
2023-03-15 23:15:20 +0530

    changed styles and added hero icon close button

--- Components/EditTodo.tsx
+++ Components/EditTodo.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { FaPen } from "react-icons/fa";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
@@ -26,7 +27,8 @@ function EditTodo({ todo }: any) {
   return (
     <>
       <button
-        className="focus:outline-none text-white bg-green-800 hover:bg-green-700 shadow-neon hover:scale-105 active:scale-75 transition-all duration-300 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-3 mr-2 mb-2 "
+        className=" text-white bg-green-800 hover:bg-green-700 shadow-neon hover:scale-110 active:scale-75 
+        transition-all duration-300 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-3 mr-2"
         onClick={() => {
           setOpenModal(true);
           setDescription(todo.description);
@@ -38,22 +40,22 @@ function EditTodo({ todo }: any) {
       {openModal ? (
         <>
           <div
-            className="fixed inset-0 bg-zinc-800/90 transition-none duration-300"
+            className="fixed inset-0 bg-zinc-800/95 transition-none  duration-300"
             onClick={() => setOpenModal(false)}
           >
             <button
-              className="absolute right-[3rem] top-[10rem] text-4xl"
+              className="absolute right-[25%] top-[30%] text-4xl"
               onClick={(e) => {
                 e.stopPropagation;
                 setOpenModal(false);
                 setDescription(todo.description);
               }}
             >
-              X
+              <AiOutlineCloseCircle className="text-white" />
             </button>
           </div>
           <div
-            className="z-10 p-5 bg-zinc-600 h-[20vh] w-[70vw] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center"
+            className="z-10 p-5 bg-zinc-900 h-[20vh] w-[70vw] lg:w-[50vw] xl:w-[40vw] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] shadow-lg shadow-purple-700 rounded-2xl flex justify-around items-center"
             onClick={(e) => {
               e.stopPropagation;
               setOpenModal(true);
@@ -61,11 +63,16 @@ function EditTodo({ todo }: any) {
           >
             <input
               type="text"
-              className="w-full h-[3rem] bg-zinc-700 p-2"
+              className="w-[80%] h-[3rem] bg-zinc-700 px-4 rounded-lg text-white font-semibold"
               value={description}
               onChange={(e) => setDescription(e.target.value)}
             />
-            <button onClick={(e) => updateDescription(e)}>Edit</button>
+            <button
+              className="max-w-sm  text-white bg-purple-700 hover:bg-purple-900  focus:ring-red-300 hover:scale-110 active:scale-75 transition-all duration-300 font-medium rounded-lg text-sm px-8 h-[3rem] mr-2"
+              onClick={(e) => updateDescription(e)}
+            >
+              Edit
+            </button>
           </div>
         </>
       ) : null}

247275bc1b402d1dc300f1f56c117e51864dae6c
Jagrath <jagrathshettylxix@gmail.com>
2023-03-15 23:16:05 +0530

    added placeholder and changed few styles

--- Components/InputTodo.tsx
+++ Components/InputTodo.tsx
@@ -29,19 +29,39 @@ function InputTodo() {
       console.log(data);
     }
   }
+
+  // async function formSubmit(e: FormEvent<HTMLFormElement>) {
+  //   e.preventDefault();
+  //   if (description === "") return;
+  //   try {
+  //     const body = { description };
+  //     const response = await fetch("http://localhost:3000/api/todos", {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify(body),
+  //     });
+  //     (window as Window).location = "/";
+  //     // (window as Window).location = "/";
+  //     router.reload();
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }
+
   return (
     <div className="w-full flex flex-col justify-around items-center  h-[20vh] ">
       <h1 className="text-3xl font-bold">Full Stack Todo List</h1>
       <form className="flex gap-5 " onSubmit={formSubmit}>
         <input
           type="text"
-          className="bg-zinc-600 px-4 rounded-sm font-semibold"
+          className="bg-zinc-600 px-4 rounded-lg  font-semibold placeholder:text-zinc-300"
           value={description}
+          placeholder="Enter to-do here!"
           onChange={(e) => {
             setDescription(e.target.value);
           }}
         />
-        <button className="flex justify-center items-center h-5 p-5 border-2 rounded-lg hover:scale-105 active:scale-95 transition-all duration-300">
+        <button className="flex justify-center items-center h-5 p-5 border-2 rounded-lg hover:scale-110 active:scale-95 transition-all duration-300">
           Add
         </button>
       </form>

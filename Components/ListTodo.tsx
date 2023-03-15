9da8ac7f87599112173005c55da6f08af15628fb
Jagrath <jagrathshettylxix@gmail.com>
2023-03-15 12:01:48 +0530

    added react icons and changed few styles

--- Components/ListTodo.tsx
+++ Components/ListTodo.tsx
@@ -1,6 +1,6 @@
-"use client ";
 import { useEffect, useState } from "react";
 import EditTodo from "./EditTodo";
+import { FaTrash } from "react-icons/fa";
 
 function ListTodo() {
   type DATA = [
@@ -44,52 +44,24 @@ function ListTodo() {
   }, []);
 
   return (
-    <div className="w-full  h-[75vh] flex flex-col justify-start items-center mt-10">
-      {/* <table className="w-full border border-seperate">
-        <thead>
-          <tr>
-            <th>DESCRIPTION</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody className="border">
-          {todos?.map((todo: loopTodo) => {
-            // console.log(i, todo["description"]);
-            return (
-              <tr key={todo.todo_id}>
-                <td className=" p-2 ">{todo.description}</td>
-                <td className=" p-2 text-center">
-                  <EditTodo todo={todo} />
-                </td>
-                <td className=" p-2 text-center">
-                  <button onClick={() => deleteTodos(todo.todo_id)}>
-                    DELETE
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table> */}
-
+    <div className="w-full  min-h-[75vh] flex flex-col justify-start items-center mt-10">
       {todos?.map((todo: loopTodo) => {
-        console.log(todo);
         return (
           <div
             key={todo.todo_id}
-            className="h-44 w-10/12 bg-white/90 rounded-lg mb-5 flex justify-between "
+            className="h-44 w-[85%] bg-purple-800 shadow-neon rounded-lg mb-5 flex  justify-between "
           >
-            <h2 className="text-2xl font-semibold text-zinc-900 pl-4 pt-10 ">
-              {todo.description.toUpperCase()}
+            <h2 className="text-2xl font-semibold text-white pl-4 pt-4 ">
+              {todo.description.slice(0, 1).toUpperCase() +
+                todo.description.slice(1)}
             </h2>
-            <div className="flex flex-col justify-between items-center px-2 py-5">
+            <div className="flex flex-col justify-between items-end px-2 py-5">
               <EditTodo todo={todo} />
               <button
                 onClick={() => deleteTodos(todo.todo_id)}
-                className="max-w-sm focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                className="max-w-sm focus:outline-none text-white bg-red-700 hover:bg-red-900 shadow-neon focus:ring-red-300 hover:scale-105 active:scale-75 transition-all duration-300 font-medium rounded-lg text-sm px-4 py-3 mr-2"
               >
-                DELETE
+                <FaTrash className="" />
               </button>
             </div>
           </div>

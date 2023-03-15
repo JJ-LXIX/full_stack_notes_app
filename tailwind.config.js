d2e041387442858964412d5bd2149fe14799a3e6
Jagrath <jagrathshettylxix@gmail.com>
2023-03-15 12:00:30 +0530

    box shadow added in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        neon: "0px 5px 10px 0px rgba(163, 26, 171, 0.8)",
+      },
+    },
   },
   plugins: [],
-}
+};

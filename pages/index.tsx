214ef48ac85bb960eff342acba72d56d94e5230f
Jagrath <jagrathshettylxix@gmail.com>
2023-03-16 10:41:49 +0530

    added background to higher parent

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,13 @@ import ListTodo from "../Components/ListTodo";
 
 const Home: NextPage = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-zinc-900 text-zinc-50">
       <Head>
         <title>Full Stack Notes App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="min-h-screen bg-zinc-900 text-zinc-50 pb-10">
+      <main className="min-h-screen  pb-10">
         <InputTodo />
         <ListTodo />
       </main>
